27d8e5acee34e18e3c12ddb5556e9d4d3a47c4d5
tolok oleksandr <tolok.oleksandr@gmail.com>
2023-01-05 16:00:47 +0100

    Updated node.js, created folder with json data, fetch data to HomeView component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,5 +9,19 @@
 
 export default {
   name: "HomeView",
+  components: {},
+
+  data() {
+    return {
+      projects: [],
+    };
+  },
+
+  mounted() {
+    fetch(" http://localhost:3000/projects")
+      .then((res) => res.json())
+      .then((data) => (this.projects = data))
+      .catch((err) => console.log(err.message));
+  },
 };
 </script>
