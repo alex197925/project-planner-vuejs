eb6e6081227bb822577c2bc141c6df21fe573546
tolok oleksandr <tolok.oleksandr@gmail.com>
2023-01-09 10:11:26 +0100

    Updating title and details with pre populated form

--- src/views/EditProjectView.vue
+++ src/views/EditProjectView.vue
@@ -3,11 +3,77 @@
 <template>
   <div>
     <h1>Edit project</h1>
+    <form>
+      <label>Title:</label>
+      <input type="text" required v-model="title" />
+      <label>Details:</label>
+      <textarea required v-model="details"></textarea>
+      <button>Update Project</button>
+    </form>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["id"],
+  data() {
+    return {
+      title: "",
+      details: "",
+      uri: " http://localhost:3000/projects/" + this.id,
+    };
+  },
+
+  mounted() {
+    fetch(this.uri)
+      .then((res) => res.json())
+      .then((data) => {
+        this.title = data.title;
+        this.details = data.details;
+      });
+  },
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+form {
+  background: white;
+  padding: 20px;
+  border-radius: 10px;
+}
+label {
+  display: block;
+  color: #bbb;
+  text-transform: uppercase;
+  font-size: 14px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  margin: 20px 0 10px 0;
+}
+input {
+  padding: 10px;
+  border: 0;
+  border-bottom: 1px solid #ddd;
+  width: 100%;
+  box-sizing: border-box;
+  outline: none;
+}
+textarea {
+  border: 1px solid #ddd;
+  padding: 10px;
+  width: 100%;
+  box-sizing: border-box;
+  height: 100px;
+  outline: none;
+}
+form button {
+  display: block;
+  margin: 20px auto 0;
+  background: #00ce89;
+  color: white;
+  padding: 10px;
+  border: 0;
+  border-radius: 6px;
+  font-size: 16px;
+}
+</style>
